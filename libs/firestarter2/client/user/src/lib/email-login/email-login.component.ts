c60202ef715fdc1e593ae2e828f9cb02a4546a0d
claudio <54946311+cvandradg@users.noreply.github.com>
2022-03-08 10:08:52 -0500

    feat: user module components

---
+++ libs/firestarter2/client/user/src/lib/email-login/email-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'material-workspace-email-login',
+  templateUrl: './email-login.component.html',
+  styleUrls: ['./email-login.component.scss']
+})
+export class EmailLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/firestarter2/client/user/src/lib/user.module.ts
+++ libs/firestarter2/client/user/src/lib/user.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { EmailLoginComponent } from './email-login/email-login.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { GoogleSigninDirective } from './google-signin/google-signin.directive';
 
 @NgModule({
   imports: [
@@ -9,6 +12,11 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
+  ],
+  declarations: [
+    EmailLoginComponent,
+    LoginPageComponent,
+    GoogleSigninDirective
   ],
 })
 export class UserModule {}
