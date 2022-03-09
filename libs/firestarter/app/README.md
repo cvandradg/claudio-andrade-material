6f3615e018b963cc61855e2bc744542d0a164817
claudio <54946311+cvandradg@users.noreply.github.com>
2022-03-09 15:36:17 -0500

    fix: Firestarter1 totally removed

--- libs/firestarter/app/README.md
+++
@@ -1,7 +0,0 @@
-# firestarter-app
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test firestarter-app` to execute the unit tests.
--- libs/firestarter/app/src/lib/home-page/home-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'material-workspace-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss'],
-})
-export class HomePageComponent {}
